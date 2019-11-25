8e8d44c88eacd39d393e827857c52a4dbcaaf941
amicablecoder9 <amanankit2498@gmail.com>
2019-11-25 22:46:40 +0530

    added path navigation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const cors = require('cors');
+const path = require('path');
 
 
 const port = process.env.PORT || 5000
@@ -8,7 +9,11 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use(cors());
-app.use(serveStatic(__dirname + '/dist/spa'))
+app.use(express.static('dist/spa'));
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'dist/spa', 'index.html'));
+});
+//app.use(serveStatic(__dirname + '/dist/spa'))
 
 const server = app.listen(port, () => {
   console.log('Listening on port ' + port)
